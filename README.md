19394d4e04d46a21f1e40d2a1aa00e567d4df178
Fiskie <fiskie@live.co.uk>
2015-01-07 17:12:20 +0000

    Update README.md
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Decode HTML entities
    
    Update for the first time in forever
    
    Update README.md
    
    Fix bugs, they've rewritten the tracker and it no longer 500s after max page so we need to fetch the page count
    
    remove word_count.txt and word_cloud.txt, add to .gitignore too
    
    fixes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# agdq-tracker-wordcloud-helper
-Scrapes AGDQ donation tracker to get word counts from the comments.
+# gdq-tracker-wordcloud-helper
+Scrapes the GDQ donation tracker to get word counts from the comments.
+
+Don't know why I made this. Cleaned it up a year later.
+
+Designed so you can slap the results of word_cloud.txt straight into wordle.
--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
-import urllib2
+import HTMLParser
 import re
+import urllib2
 
 pageExists = 1
 page = 0
@@ -7,104 +8,112 @@ page = 0
 # Seemed to be 403ing without a user agent, let's fix that :^)
 
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-headers = { 'User-agent' : user_agent }
+headers = {'User-agent': user_agent}
 
-eventId = int(raw_input('Enter event ID (12 for AGDQ 2015): '));
-maxPages = int(raw_input('Enter number of pages to parse (0 for all): '));
+eventId = raw_input('Enter event ID (12 for AGDQ 2015): ')
+pageLimit = int(raw_input('Enter number of pages to parse (0 for all): '))
 
 commentRegex = re.compile('<td class="(?:Invalid Variable: )?commentstate">\n(.*)(?:<hr>)\n<\/td>')
-donorListRegex = re.compile('<tr>\n<td>\n<a href="(.*)">(.*)</a>\n<\/td>\n<td>\n<span class="datetime">(?:.*)</span>\n<\/td>\n<td>\n<a href="(.*)">(.*)<\/a>\n<\/td>\n<td>\nYes\n<\/td>\n<\/tr>')
+donorListRegex = re.compile('<a href="\/tracker\/donor\/.+">(.*)</a>\n<\/td>\n<td>\n<span class="datetime">(?:.*)</span>\n<\/td>\n<td>\n<a href="(.*)">(.*)<\/a>')
+lastPageRegex = re.compile('<a href="\?page=(\d+)" class="last">')
 
 stringMap = {}
+htmlParser = HTMLParser.HTMLParser()
+
 
 # Parse a donor preg object
 
-def parseDonor(donor):
 
-	# 0: donor profile page. We don't want this, it'll result in duplicates.
-	# 1: Username
-	# 2: personal donation page.
-	# 3: donation amount in the format $xx.xx.
+def parse_donor(donor):
+    """
+    # 0: Username
+    # 1: personal donation page.
+    # 2: donation amount in the format $xx.xx.
+    
+    :param donor: list
+    """
 
-	print 'Getting ' + donor[1] + '\'s donation message...'
-	
-	url = 'https://gamesdonequick.com' + donor[2]
-	req = urllib2.Request(url, None, headers)
-	response = urllib2.urlopen(req)
+    url = 'https://gamesdonequick.com' + donor[1]
+    req = urllib2.Request(url, None, headers)
+    response = urllib2.urlopen(req)
 
-	html = response.read()
+    html = response.read()
 
-	matches = commentRegex.findall(html)
+    matches = commentRegex.findall(html)
 
-	if len(matches) == 0:
-		print 'Comment pending or rejected.'
-		return
+    if len(matches) == 0:
+        # Comment was rejected
+        return
 
-	# We have a comment, let's split the words and map them.
+    # We have a comment, let's split the words and map them.
 
-	comment = matches[0]
+    comment = htmlParser.unescape(matches[0])
 
-	print 'Comment: ' + comment
+    print('%20s: %s' % (donor[0], comment))
 
-	addToMap(comment)
+    add_to_cloud(comment)
 
-# Add a comment to the map
 
-def addToMap(comment):
-	stringList = comment.split(' ')
-	
-	for string in stringList:
-		string = string.lower()
+def add_to_cloud(comment):
+    """
+    Add a comment to the map.
+    :param comment: str
+    """
+    
+    for word in comment.split(' '):
+        word = word.lower()
 
-		if string in stringMap:
-			stringMap[string] += 1
-		else:
-			stringMap[string] = 1
+        if word in stringMap:
+            stringMap[word] += 1
+        else:
+            stringMap[word] = 1
 
 # Main loop
 
-while pageExists:
-	page += 1
+maxPage = 1
 
-	if maxPages != 0 and page > maxPages:
-		print 'Page limit of ' + str(maxPages) + ' reached, quitting.'
-		break;
+while maxPage > page:
+    page += 1
 
-	print 'Downloading page ' + str(page) + '...'
+    if pageLimit != 0 and page > pageLimit:
+        print('Page limit of %d reached, stopping.' % pageLimit)
+        break
 
-	url = 'https://gamesdonequick.com/tracker/donations/' + str(eventId) + '?page=' + str(page)
+    print('Downloading page %d of %d...' % (page, maxPage))
 
-	req = urllib2.Request(url, None, headers)
-	
-	try:
-		response = urllib2.urlopen(req)
-	except urllib2.HTTPError:
-		print 'Internal server error, assumed finished.'
-		break
+    url = 'https://gamesdonequick.com/tracker/donations/%s?page=%d' % (eventId, page)
 
-	html = response.read()
+    req = urllib2.Request(url, None, headers)
+    
+    try:
+        response = urllib2.urlopen(req)
+    except urllib2.HTTPError:
+        print('Internal server error. Stopping.')
+        break
 
-	donorsWithComments = donorListRegex.findall(html)
+    html = response.read()
 
-	for donor in donorsWithComments:
-		parseDonor(donor)
+    maxPage = int(lastPageRegex.findall(html)[0])
+
+    for donor in donorListRegex.findall(html):
+        parse_donor(donor)
 
 # Save stringMap data to file
 
-print 'Saving lists...'
+print('Saving lists...')
 
 wordCount = open('word_count.txt', 'w')
 wordCloud = open('word_cloud.txt', 'w')
 
 for string in stringMap:
-	count = stringMap[string]
+    count = stringMap[string]
 
-	wordCount.write(string + ' ' + str(count) + '\n')
+    wordCount.write(string + ' ' + str(count) + '\n')
 
-	for i in range (0, count - 1):
-		wordCloud.write(string + ' ')
+    for i in range (0, count - 1):
+        wordCloud.write(string + ' ')
 
 wordCount.close()
 wordCloud.close()
 
-print 'Done'
+print('Done')
